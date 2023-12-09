44d997389ad9dc0bf985523f9e678d05f4955ef9
Mineta <102620985+MinetaMexhiti@users.noreply.github.com>
2023-12-09 01:41:33 +0100

    Add files via upload

---
+++ SingletonPattern_5/SingletonPattern_5/Logger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace SingletonPattern_5
+{
+    public class Logger
+    {
+        private static Logger? instance_;
+        private static readonly object lock_ = new object();
+        private StreamWriter _streamWriter;
+
+        private Logger()
+        {
+            try
+            {
+                _streamWriter = new StreamWriter("application.log", true);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Logger initialization failed: {ex.Message}");
+            }
+        }
+
+        public static Logger Inst
+        {
+            get
+            {
+                lock (lock_)
+                {
+                    if (instance_ == null)
+                    {
+                        instance_ = new Logger();
+                    }
+                    return instance_;
+                }
+            }
+        }
+
+        public void Log(string message)
+        {
+            try
+            {
+                _streamWriter.WriteLine($"{DateTime.Now}: {message}");
+                _streamWriter.Flush();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to write log: {ex.Message}");
+            }
+        }
+
+        public void Dispose()
+        {
+            _streamWriter?.Close();
+        }
+    }
+}
---
+++ SingletonPattern_5/SingletonPattern_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SingletonPattern_5;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        Logger logger = Logger.Inst;
+
+
+        logger.Log("Application started.");
+        logger.Log("Performing some operations...");
+
+
+        logger.Log("Application end . ");
+
+        logger.Dispose();
+             
+    }
+}
